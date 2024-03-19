6289d3caa136eb8b04ba0034b9fb8656b4a34c13
grasume <jamesmgerstenberg@gmail.com>
2024-03-18 21:20:19 -0500

    first commit

---
+++ Modules/VPC/main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = "true"
+  routing_mode            = "GLOBAL"
+}
+
+resource "google_compute_firewall" "firewall" {
+  name    = "firewall-rule-name"
+  network = var.vpc_name  
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
